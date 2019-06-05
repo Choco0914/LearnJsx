e66c198e3fee9018c2e8f2121f615462a8aa394c
Choco0914 <heojeongho1992@gmail.com>
2019-06-05 12:05:48 +0900

    Building Content with JSX

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Learn JSX
+
+## To do list
+
+1.  Let's Dive in! [x]
+
+2.  Building Content with JSX [x]
--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import ReactDOM from "react-dom";
 // Create a react component
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "눌러주세요" };
+  const labelText = "이름을 입력하세요:";
+
+  return (
+    <div>
+      <label class="label" for="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
